f2a0270440f09affdcb7914da6382947c660c427
Riley Seaburg <riley@rds.marketing>
2020-04-14 16:44:10 -0400

    added api endpoints

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Connected to: ${db}`);
+  } catch (error) {
+    console.log(error);
+    //   Fail
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
